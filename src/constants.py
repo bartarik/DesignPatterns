6882ca5436f3e32c8212407bb66898a049b6fbb6
Tarik Barucija <barucija@student.tugraz.at>
2023-10-30 00:33:33 +0100

    Created the board, defined some first steps for the arhitecture, initial setup of the project done

---
+++ src/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+# Game dimensions
+WIDTH, HEIGHT = 800, 800
+BOARD_ROWS, BOARD_COLS = 8, 8
+SQUARE_SIZE = WIDTH // BOARD_COLS
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
